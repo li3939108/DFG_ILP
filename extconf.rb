c06ea9df5a7092a646f8f34105e95066a9cdf10e
li3939108 <li3939108@tamu.edu>
2013-11-12 20:46:28 -0600

    commonts on dif_config

--- extconf.rb
+++ extconf.rb
@@ -2,9 +2,11 @@ require 'mkmf'
 
 #have_library()
 
-#ruby extconf.rb --with-DFG_ILP-dir=.
-#set the lib and include directory associated with DFG_ILP to ./lib ./include
-dir_config('DFG_ILP')
+#ruby extconf.rb --with-DFG_ILP-dir=dir
+#set the lib and include directory associated with DFG_ILP to dir/lib dir/include
+#ruby extconf.rb
+#will set the lib and include directory associated with DFG_ILP to ./lib and ./include
+dir_config('DFG_ILP', '.')
 
 #create it
 create_makefile('DFG_ILP')
