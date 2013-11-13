8daacd1d4fecaff740223d187f3665a04722c5f2
li3939108 <li3939108@tamu.edu>
2013-11-12 20:11:23 -0600

    rm IIR_graph

--- IIR_graph.rb
+++
@@ -1,6 +0,0 @@
-section1 = {"preceding"=>[{"preceding"=>[{"preceding"=>[{"preceding"=>[{"preceding"=>[], "type"=>"x"}, {"preceding"=>[], "type"=>"x"}], "type"=>"+"}, {"preceding"=>[], "type"=>"x"}], "type"=>"+"}, {"preceding"=>[], "type"=>"x"}], "type"=>"+"}], "type"=>"+"}
-section2 = {"preceding"=>[{"preceding"=>[{"preceding"=>[{"preceding"=>[{"preceding"=>[], "type"=>"x"}, {"preceding"=>[], "type"=>"x"}], "type"=>"+"}, {"preceding"=>[], "type"=>"x"}], "type"=>"+"}, {"preceding"=>[], "type"=>"x"}], "type"=>"+"}], "type"=>"+"} 
-section3 = {"preceding"=>[{"preceding"=>[{"preceding"=>[{"preceding"=>[{"preceding"=>[], "type"=>"x"}, {"preceding"=>[], "type"=>"x"}], "type"=>"+"}, {"preceding"=>[], "type"=>"x"}], "type"=>"+"}, {"preceding"=>[], "type"=>"x"}], "type"=>"+"}], "type"=>"+"} 
-section3['preceding'][0]['preceding'][0]['preceding'][0]['preceding'][0]['preceding'] = [section2]
-section2['preceding'][0]['preceding'][0]['preceding'][0]['preceding'][0]['preceding'] = [section1]
-fullgraph  = section3 
