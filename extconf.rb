ca2dcadc7a6c9f3c898aa98ea30921103a9ba351
li3939108 <li3939108@tamu.edu>
2013-11-12 20:48:59 -0600

    more comment on dir_config

--- extconf.rb
+++ extconf.rb
@@ -6,6 +6,8 @@ require 'mkmf'
 #set the lib and include directory associated with DFG_ILP to dir/lib dir/include
 #ruby extconf.rb
 #will set the lib and include directory associated with DFG_ILP to ./lib and ./include
+#ruby extconf.rb --with-DFG_ILP-lib=lib will set the lib directory to lib
+#ruby extconf.rb --with-DFG_ILP-include=include will set the include directory to include
 dir_config('DFG_ILP', '.')
 
 #create it
