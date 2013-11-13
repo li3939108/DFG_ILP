f1a5207e98821b70ad6d66a3967357053b609f6b
li3939108 <li3939108@tamu.edu>
2013-11-13 14:37:28 -0600

    hand modified Makefile, working well, interface between ILP and ruby done

--- DFG_ILP.c
+++ DFG_ILP.c
@@ -3,61 +3,7 @@
 #include "lp_lib.h"
 #include "ruby.h"
 
-#define DISPLAY
-
-int main()
-{
-	lprec *lp;
-	int column = 2;
-	REAL row[1+column] ;
-
-	lp = make_lp(0,column);
-	set_maxim(lp);
-
-	if(lp == NULL) {
-		perror("Unable to create new LP model\n");
-		exit(EXIT_FAILURE);
-	}
-
-	set_add_rowmode(lp, true);
-
-	row[1] = -1.0; row[2] = 1.0;
-	add_constraint(lp, row, LE, 1.0); 
-	row[1] =  3.0; row[2] = 2.0;
-	add_constraint(lp, row, LE, 12.0); 
-	row[1] =  2.0; row[2] = 3.0;
-	add_constraint(lp, row, LE, 12.0); 
-
-	set_add_rowmode(lp, false);
-	
-	set_int(lp, 1, true);	
-	set_int(lp, 2, true);	
-
-	row[1] = 0.0; row[2] = 1.0;
-	set_obj_fn(lp, row);
-
-	solve(lp);
-
-	REAL result[1+get_Nrows(lp)+get_Ncolumns(lp)];
-	int i;
-	
-	get_primal_solution(lp, result);
-
-#ifdef DISPLAY
-	for(i = 0; i < 1+get_Nrows(lp)+get_Ncolumns(lp); i++){
-		if(i == 0){
-			printf("obj: ");
-		}else{if(i >= 1 && i <= get_Nrows(lp)){
-			printf("constraints %d: ", i);
-		}else{if(i >= 1 + get_Nrows(lp) && i <= get_Nrows(lp) + get_Ncolumns(lp)){
-			printf("variable %d: ", i - get_Nrows(lp));
-		}}}
-		printf("%f\n", result[i]);
-	}
-#endif
-	delete_lp(lp);
-	return 0;
-}
+//#define DISPLAY
 
 static VALUE ILP(VALUE self, VALUE A, VALUE op, VALUE b, VALUE c, VALUE min){
 	Check_Type(A, T_ARRAY) ;
@@ -71,11 +17,12 @@ static VALUE ILP(VALUE self, VALUE A, VALUE op, VALUE b, VALUE c, VALUE min){
 	REAL row[1 + Ncolumn] ;
 	REAL result[1 + Nrow + Ncolumn];
 
-	VALUE ret_hash;
+	VALUE ret_hash = rb_hash_new();
 	VALUE constraints = rb_ary_new2(Nrow);
 	VALUE variables = rb_ary_new2(Ncolumn);
 
-	lp = make_lp(Nrow, Ncolumn);
+	lp = make_lp(0, Ncolumn);
+	set_verbose(lp, SEVERE);
 
 	if(TYPE(min) == T_TRUE){
 		set_minim(lp);
@@ -113,6 +60,7 @@ static VALUE ILP(VALUE self, VALUE A, VALUE op, VALUE b, VALUE c, VALUE min){
 	
 	for(i = 0; i < Ncolumn; i++){
 		row[i + 1] = NUM2DBL(rb_ary_entry(c, i));
+		set_int(lp, i + 1, true);
 	}
 	
 	set_obj_fn(lp, row);
@@ -120,20 +68,32 @@ static VALUE ILP(VALUE self, VALUE A, VALUE op, VALUE b, VALUE c, VALUE min){
 	solve(lp);
 
 	get_primal_solution(lp, result);
-	
-	delete_lp(lp);
+#ifdef DISPLAY
+	for(i = 0; i < 1+get_Nrows(lp)+get_Ncolumns(lp); i++){
+		if(i == 0){
+			printf("obj: ");
+		}else{if(i >= 1 && i <= get_Nrows(lp)){
+			printf("constraints %d: ", i);
+		}else{if(i >= 1 + get_Nrows(lp) && i <= get_Nrows(lp) + get_Ncolumns(lp)){
+			printf("variable %d: ", i - get_Nrows(lp));
+		}}}
+		printf("%f\n", result[i]);
+	}
+#endif	
 
-	for(i = 1; i < 1+get_Nrows(lp)+get_Ncolumns(lp); i++){
+	for(i = 1; i < 1 + get_Nrows(lp) + get_Ncolumns(lp); i++){
 		if(i >= 1 && i <= get_Nrows(lp)){
 			rb_ary_store(constraints, i - 1, rb_float_new(result[i]));
 		}else{if(i >= 1 + get_Nrows(lp) && i <= get_Nrows(lp) + get_Ncolumns(lp)){
-			rb_ary_store(variables, i - 1 - get_Nrows(lp), rb_float_new(result[i]));
+			rb_ary_store(variables, i - 1 - get_Nrows(lp), INT2NUM((int)result[i]));
 		}}
 	}
-	rb_hash_aset(ret_hash, ID2SYM(rb_intern("obj")), rb_float_new(result[0]));
-	rb_hash_aset(ret_hash, ID2SYM(rb_intern("constraints")), constraints);
-	rb_hash_aset(ret_hash, ID2SYM(rb_intern("variables")), variables);
+
+	rb_hash_aset(ret_hash, ID2SYM(rb_intern("o")), rb_float_new(result[0]));
+	rb_hash_aset(ret_hash, ID2SYM(rb_intern("c")), constraints);
+	rb_hash_aset(ret_hash, ID2SYM(rb_intern("v")), variables);
 	
+	delete_lp(lp);
 	return ret_hash;
 
 }
--- extconf.rb
+++ extconf.rb
@@ -1,14 +1,14 @@
 require 'mkmf'
 
-have_library('lpsolve55')
 
-#ruby extconf.rb --with-DFG_ILP-dir=dir
-#set the lib and include directory associated with DFG_ILP to dir/lib dir/include
-#ruby extconf.rb
-#will set the lib and include directory associated with DFG_ILP to ./lib and ./include
-#ruby extconf.rb --with-DFG_ILP-lib=lib will set the lib directory to lib
-#ruby extconf.rb --with-DFG_ILP-include=include will set the include directory to include
-dir_config('DFG_ILP', '.')
+#have_library('lpsolve55') find_library('lpsolve55', '_make_lp', './lib') and 
+	#ruby extconf.rb --with-DFG_ILP-dir=dir
+	#set the lib and include directory associated with DFG_ILP to dir/lib dir/include
+	#ruby extconf.rb
+	#will set the lib and include directory associated with DFG_ILP to ./lib and ./include
+	#ruby extconf.rb --with-DFG_ILP-lib=lib will set the lib directory to lib
+	#ruby extconf.rb --with-DFG_ILP-include=include will set the include directory to include
+	dir_config('DFG_ILP', './include', '/Domain/continuum.tamu.edu/Users/li3939108/lib/')
 
-#create it
-create_makefile('DFG_ILP')
+	#create it
+	create_makefile('DFG_ILP')
