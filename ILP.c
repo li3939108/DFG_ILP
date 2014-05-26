1cd68d24a272e3be169ad60e4c68d0d4ad1c1c5f
me <li3939108@gmail.com>
2014-05-26 07:47:44 -0500

    as_possible moved to separated file

--- ILP.c
+++ ILP.c
@@ -6,7 +6,8 @@
 #include <string.h>
 #include "ruby.h"
 
-#include "graph.h"
+//#include "graph.h"
+#include "as_possible.h"
 
 #define round(x) ((x)>=0?(long)((x)+0.5):(long)((x)-0.5))
 
@@ -16,73 +17,12 @@
 #define EQ 3
 #endif
 
-#define DEBUG
+//#define DEBUG
 
 
 VALUE cGraph ;
 VALUE graph_obj ;
 VALUE reverse_graph_obj ;
-
-
-void get_graph(VALUE vlist, VALUE elist) {
-	Graph *G, *Gt ;
-	int i, elist_len = RARRAY_LEN(elist), vlist_len = RARRAY_LEN(vlist);
-	Vertex *vertex_list[vlist_len] ;
-	char vertices[vlist_len] ;
-	Check_Type(vlist, T_ARRAY) ;
-	Check_Type(elist, T_ARRAY) ;	
-	memset(vertex_list, 0, sizeof vertex_list);
-	for(i = 0; i < vlist_len; i++){
-		int t ;
-		VALUE op = rb_ary_entry(vlist, i) ;
-		char *operation ;
-		Check_Type(op, T_STRING) ;
-		RSTRING_GETMEM(op, operation, t) ;
-		vertices[i] = operation[0] ;
-	}
-	for(i = 0; i < elist_len; i++){
-		VALUE edge = rb_ary_entry(elist, i) ;
-		int e1, e2 ;
-		Check_Type(edge, T_ARRAY) ;
-		e1 = FIX2INT(rb_ary_entry(edge, 1) ) ;
-		e2 = FIX2INT(rb_ary_entry(edge, 0) );
-		if(vertex_list[e1] == NULL){
-			vertex_list[e1] = new_vertex(e1 + 1) ;
-			vertex_list[e1]->op = vertices[e1] ;
-		}
-		if(vertex_list[e2] == NULL){
-			vertex_list[e2] = new_vertex(e2 + 1) ;
-			vertex_list[e2]->op = vertices[e2] ;
-		};
-		add_adjacency_vertex(vertex_list[e1], e2 + 1, vertices[e2]) ;
-	}
-	G = new_graph(vlist_len, vertex_list);
-	Gt = reverse(G) ;
-	graph_obj = Data_Wrap_Struct(cGraph, 0, free_graph, G) ;
-	reverse_graph_obj = Data_Wrap_Struct(cGraph, 0, free_graph, Gt) ;
-}
-int dfs(Graph *G, int s_label, int time[], VALUE delay, char asap_or_alap){//no cycles
-	int i ;
-	char op[2] ;
-	VALUE d_arr ;
-	if(time[s_label] != -1){
-		return time[s_label] ;
-	}
-	for(i = 0; i < G->adj_list[s_label]->degree; i++){
- 		int *adj = (int *)G->adj_list[ s_label ]->list[ i ] ;
- 		int time_step = dfs(G, adj[0], time, delay, asap_or_alap) ;
- 		time[s_label] = time[s_label] < time_step ? time_step : time[s_label] ;
-	}
-	op[0] = G->adj_list[s_label]->op ;
-	op[1] = '\0' ;
-	d_arr = rb_hash_aref(delay, rb_str_new2(op) ) ;
-	if(asap_or_alap == 'S' || asap_or_alap == 's'){
-		time[s_label] = time[s_label] + FIX2INT( rb_ary_entry( d_arr, 0) );
-	}else if(asap_or_alap == 'L' || asap_or_alap == 'l'){
-		time[s_label] = time[s_label] + FIX2INT( rb_ary_entry( d_arr, RARRAY_LEN(d_arr) - 1) );
-	}
-	return time[s_label] ;
-}
 
 static VALUE ASAP(VALUE self){
 	Graph *G, *Gt ;
@@ -102,10 +42,7 @@ static VALUE ASAP(VALUE self){
 	}
 	time = calloc(G->V + 1, sizeof *time);
 	memset(time, 0xFF, (G->V + 1) * sizeof *time) ; //set all entry -1
-
-	for(i = 1; i <= G->V; i++){
-		dfs(Gt, i, time, delay, 's') ;
-	}
+	asap(Gt, time, delay ) ;
 	for(i = 1; i <= G->V; i++){
 		VALUE d_arr, sch = rb_hash_new() ;
 		rb_hash_aset(sch, ID2SYM(rb_intern("id")), INT2FIX( i ) );
@@ -135,7 +72,7 @@ static VALUE ALAP(VALUE self){
 	VALUE vlist = rb_ivar_get(self, rb_intern("@vertex") );
 	VALUE elist = rb_ivar_get(self, rb_intern("@edge") );
 	VALUE delay = rb_ivar_get(self, rb_intern("@d") ) ;
-	int *time, i, Q = -1;
+	int *time, i ;
 	VALUE ret = rb_ary_new();
 
 	Data_Get_Struct(graph_obj, Graph, G) ;
@@ -147,14 +84,8 @@ static VALUE ALAP(VALUE self){
 	}
 	time = calloc(G->V + 1, sizeof *time);
 	memset(time, 0xFF, (G->V + 1) * sizeof *time) ; //set all entry -1
+	alap(G, time, delay) ;
 
-	for(i = 1; i <= G->V; i++){
-		int new_Q = dfs(G, i, time, delay, 'L') ;
-		Q = new_Q > Q ? new_Q : Q ;
-	}
-	for(i = 1; i <= G->V; i++){
-		time[i] = Q - time[i] ;
-	}
 	for(i = 1; i <= G->V; i++){
 		VALUE d_arr, sch = rb_hash_new();
 		rb_hash_aset(sch, ID2SYM(rb_intern("id")), INT2FIX( i ) );
