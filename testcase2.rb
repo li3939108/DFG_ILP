2c487d33ee4b3ccacbb34db6d29baebedb12a605
me <li3939108@gmail.com>
2014-05-27 18:28:55 -0500

    update

--- testcase2.rb
+++ testcase2.rb
@@ -5,7 +5,10 @@ delay =       {'+' => [1, 2],         'x' => [2, 3],          'D' => [1]} #delay
 g = DFG_ILP::GRAPH.new
 g.IIR(4)
 ret = g.ASAP(delay)
+print "ASAP: ",ret,"\n"
 #DFG_ILP::GRAPH.vs(ret, 0)
 ret = g.ALAP(delay)
+print "ALAP: ",ret,"\n"
 #DFG_ILP::GRAPH.vs(ret, 0)
-g.M(delay)
+ret = g.M(delay)
+print "Mobility: ", ret, "\n"
