8095e667063dff132157c34dd1eaee7576593dd6
me <li3939108@gmail.com>
2014-05-27 03:22:44 -0500

    bug nil.to_i = 0

--- testcase.rb
+++ testcase.rb
@@ -4,6 +4,6 @@ require './DFG_ILP.rb'
 g = DFG_ILP::GRAPH.new
 g.IIR(4)
 g.dot(File.open("tmp.dot", "w") )
-ilp = DFG_ILP::ILP.new(g, ARGV[0].to_i)
+ilp = DFG_ILP::ILP.new(g, ARGV[0] == nil ? nil : ARGV[0].to_i)
 r = ilp.compute(g)
 DFG_ILP::GRAPH.vs(r[:sch], 0)
