300ac8500d0f30390179cd86038ec95f34f108aa
me <li3939108@gmail.com>
2014-05-27 02:37:44 -0500

    bug nil.to_i is 0

--- testcase2.rb
+++ testcase2.rb
@@ -5,7 +5,7 @@ delay =       {'+' => [1, 2],         'x' => [2, 3],          'D' => [1]} #delay
 g = DFG_ILP::GRAPH.new
 g.IIR(4)
 ret = g.ASAP(delay)
-DFG_ILP::GRAPH.vs(ret, 0)
+#DFG_ILP::GRAPH.vs(ret, 0)
 ret = g.ALAP(delay)
-DFG_ILP::GRAPH.vs(ret, 0)
+#DFG_ILP::GRAPH.vs(ret, 0)
 g.M(delay)
