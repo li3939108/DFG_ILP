05eb3d7db6c8c03754c15b6a81cce974a0e8521d
me <li3939108@gmail.com>
2014-11-08 11:56:57 -0600

    new test

--- test/test.rb
+++ test/test.rb
@@ -31,35 +31,46 @@ sds.ifactor
 testcase = [sds]
 testset = [iir4, arf, mv, mm, pyr, jbmp, sds]
 
-minLatency = [14, 11, 7, 11, 8, 8, 17]
+minLatency = {
+	iir4 =>14, 
+	arf => 11, 
+	mv => 7, 
+	mm => 11, 
+	pyr => 8, 
+	jbmp => 8, 
+	sds => 17,
+}
 
-testcase.each do |g|
+
+testset.each do |g|
+	latency = minLatency[g] * 2
+	variance_bound = 30000
 	print "\n", g.p[:name], " start", "\n---------------------------\n"
-	ilp = DFG_ILP::ILP.new(g, {:err_type => 'var', :q => ARGV[0].to_i, :variance_bound => ARGV[1].to_i}) 
+	ilp = DFG_ILP::ILP.new(g, {:err_type => 'var', :q => latency, :variance_bound => variance_bound}) 
 	ret = ilp.ASAP
 	print   "ASAP scheduling done, longest path length: ", ret[:latency], "\n"
 	r = ilp.compute(g, :cplex)
 	
-	print "var: ", r[:var].map{|var_slack| ARGV[1].to_i - var_slack}, "\n"
+	print "var: ", r[:var].map{|var_slack| variance_bound - var_slack}, "\n"
 	er_bound = r[:sch].select{|sch| g.p[:PO][sch[:id] - 1] }.map{|schedule| schedule[:error] }.max
 
 	ilp.vs(r[:sch], 0)
 
 	print 'er bound: ', 1 - Math::E**er_bound
-	er_ilp = DFG_ILP::ILP.new(g, {:q => ARGV[0].to_i, :error_bound => er_bound}) 
+	er_ilp = DFG_ILP::ILP.new(g, {:q => latency, :error_bound => er_bound}) 
 	er_r = er_ilp.compute(g, :cplex)
 	print "var: ", er_r[:var].map{|var| -var}, "\n"
 
 	er_ilp.vs(er_r[:sch], 0)
 
 	# all approximate	
-	full_approximate_ilp = DFG_ILP::ILP.new(g, {:err_type => 'var', :q => ARGV[0].to_i, :variance_bound =>99999999 })
+	full_approximate_ilp = DFG_ILP::ILP.new(g, {:err_type => 'var', :q => latency, :variance_bound =>99999999 })
 	fa_ret = full_approximate_ilp.compute(g, :cplex)
 	print "var: ", fa_ret[:var].map{|var_slack| 99999999 - var_slack}, "\n"
 	full_approximate_ilp.vs(fa_ret[:sch], 0)
 
 	#accurate 
-	accurate_ilp = DFG_ILP::ILP.new(g, {:err_type => 'var', :q => ARGV[0].to_i, :variance_bound => 0})
+	accurate_ilp = DFG_ILP::ILP.new(g, {:err_type => 'var', :q => latency, :variance_bound => 0})
 	a_ret = accurate_ilp.compute(g, :cplex)
 	accurate_ilp.vs(a_ret[:sch], 0)
 end
