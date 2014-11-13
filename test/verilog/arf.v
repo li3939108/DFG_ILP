49f39ce97bb5f32cbc6a485494b90ceb83631682
me <li3939108@gmail.com>
2014-11-13 11:30:48 -0600

    bug of arf.v

--- test/verilog/arf.v
+++ test/verilog/arf.v
@@ -163,14 +163,14 @@ initial begin
 end
 
 initial begin
-	in_1_0 = {$urandom(),$urandom()} >> 48
-	in_2_0 = {$urandom(),$urandom()} >> 48
-	in_3_0 = {$urandom(),$urandom()} >> 48
-	in_4_0 = {$urandom(),$urandom()} >> 48
-	in_5_0 = {$urandom(),$urandom()} >> 48
-	in_6_0 = {$urandom(),$urandom()} >> 48
-	in_7_0 = {$urandom(),$urandom()} >> 48
-	in_8_0 = {$urandom(),$urandom()} >> 48
+	in_1_0 = {$urandom(),$urandom()} >> 48;
+	in_2_0 = {$urandom(),$urandom()} >> 48;
+	in_3_0 = {$urandom(),$urandom()} >> 48;
+	in_4_0 = {$urandom(),$urandom()} >> 48;
+	in_5_0 = {$urandom(),$urandom()} >> 48;
+	in_6_0 = {$urandom(),$urandom()} >> 48;
+	in_7_0 = {$urandom(),$urandom()} >> 48;
+	in_8_0 = {$urandom(),$urandom()} >> 48;
 end
 
 endmodule
