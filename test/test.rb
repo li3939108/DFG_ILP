c71e68197f33399037cfa3314b46ca7ad33c97d1
Chaofan Li <li3939108@gmail.com>
2014-11-23 09:49:03 -0600

    test.rb updated

--- test/test.rb
+++ test/test.rb
@@ -2,6 +2,67 @@
 
 require 'dfg_ilp'
 
+operation_parameters2 = {
+
+	's' => {
+		:type => ["approximate", "accurate"], 
+		:u    => [1, 1], 
+		:d    => [3, 3], 
+		:g    => [50000, 142200],
+		:p    => [300000, 430000],
+		:err  => [Math::log(1 - 0.91), Math::log(1 - 0)] ,
+		:variance  => [210000, 0],
+	},
+	'x' => {
+		:type => ["approximate", "accurate"], 
+		:u    => [1, 1], 
+		:d    => [3, 3], 
+		:g    => [50000, 142200],
+		:p    => [300000, 430000],
+		:err  => [Math::log(1 - 0.91), Math::log(1 - 0)] ,
+		:variance  => [210000, 0],
+	},
+	'ALU' => {
+		:type => ["32/8trun", "32/8appr", "32/4trun","accurate"], 
+		:u    => [1, 1, 1, 1],
+		:d    => [2, 2, 2, 2], 
+		:g    => [48390, 38750, 55670,66780],
+		:p    => [7300,  4000, 8500, 10000],
+		:err  => [Math::log(1 - 1), Math::log(1- 0.996), Math::log(1 - 0.91),Math::log(1 - 0)], 
+		:variance  => [10905, 6833, 42, 0],
+	},
+	'+' => {
+		:type => ["32/8trun", "32/8appr", "32/4trun","accurate"], 
+		:u    => [1, 1, 1, 1],
+		:d    => [2, 2, 2, 2], 
+		:g    => [48390, 38750, 55670,66780],
+		:p    => [7300,  4000, 8500, 10000],
+		:err  => [Math::log(1 - 1), Math::log(1- 0.996), Math::log(1 - 0.91),Math::log(1 - 0)], 
+		:variance  => [10905, 6833, 42, 0],
+	},
+	'D' => {
+		:type => ["accurate"],
+		:u    => [Float::INFINITY],
+		:d    => [1],
+		:g    => [0],
+		:p    => [0],
+		:err  => [Math::log(1 - 0)] ,
+		:variance  => [0],
+
+	},
+	'@' => {
+		:type => ["accurate"],
+		:u    => [Float::INFINITY],
+		:d    => [1],
+		:g    => [6050],
+		:p    => [4],
+		:err  => [Math::log(1 - 0)] ,
+		:variance  => [0],
+	},
+	}
+
+
+
 operation_parameters1 = {
 
 	's' => {
@@ -63,7 +124,7 @@ operation_parameters1 = {
 
 	
 
-root_dir = "/homes/grad/li3939108/DFG_ILP"
+root_dir = "/home/me/DFG_ILP"
 
 arf = DFG_ILP::Parser.new("#{root_dir}/test/dot/arf.dot").parse.to_DFG
 arf.ifactor
@@ -105,6 +166,7 @@ minLatency = {
 
 
 testcase.each do |g|
+	operation_parameters = operation_parameters2
 	latency = minLatency[g] * 2
 	variance_bound = 30000
 	$stderr.print "\n", g.p[:name], " start", "\n---------------------------\n"
@@ -113,7 +175,7 @@ testcase.each do |g|
 		:err_type => 'var', 
 		:q => latency, 
 		:variance_bound => variance_bound, 
-		:operation_parameters => operation_parameters1} )
+		:operation_parameters => operation_parameters} )
 	ret = ilp.ASAP(:min)
 	$stderr.print   "ASAP scheduling done, longest path length: ", ret[:latency], "\n"
 	r = ilp.compute(g, :cplex)
@@ -130,7 +192,7 @@ testcase.each do |g|
 	er_ilp = DFG_ILP::ILP.new(g, {
 		:q => latency, 
 		:error_bound => er_bound,
-		:operation_parameters => operation_parameters1}) 
+		:operation_parameters => operation_parameters}) 
 	er_r = er_ilp.compute(g, :cplex)
 	$stderr.print "var: ", er_r[:var].map{|var| -var}, "\n"
 	max_var = er_r[:var].map{|var| -var}.max
@@ -144,7 +206,7 @@ testcase.each do |g|
 		:err_type => 'var', 
 		:q => latency, 
 		:variance_bound =>99999999,
-		:operation_parameters => operation_parameters1 })
+		:operation_parameters => operation_parameters })
 	fa_ret = full_approximate_ilp.compute(g, :cplex)
 	$stderr.print "var: ", fa_ret[:var].map{|var_slack| 99999999 - var_slack}, "\n"
 	max_var = fa_ret[:var].map{|var_slack| 99999999 - var_slack}.max
@@ -158,7 +220,7 @@ testcase.each do |g|
 		:err_type => 'var', 
 		:q => latency, 
 		:variance_bound => 0, 
-		:operation_parameters => operation_parameters1})
+		:operation_parameters => operation_parameters})
 	a_ret = accurate_ilp.compute(g, :cplex)
 	accurate_ilp.vs(a_ret[:sch], 0)
 	
@@ -169,7 +231,7 @@ testcase.each do |g|
 		:type => 'mmkp', 
 		:variance_bound => variance_bound, 
 		:q => latency,
-		:operation_parameters => operation_parameters1})
+		:operation_parameters => operation_parameters})
 	mmkp_r = ilp.mmkp_compute(g, :cplex)
 	max_var =  mmkp_r[:var].map{|var_slack| variance_bound - var_slack}.max
 	er_bound = [*0..g.p[:v].length - 1].select{|i| g.p[:PO][i] }.map{|po| mmkp_r[:error][po] }.max
