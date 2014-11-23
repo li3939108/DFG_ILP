c846817a75cf3973a786f74f95b03ce432adf6e1
me@hera.ece <li3939108@gmail.com>
2014-11-23 12:46:43 -0600

    scalers

--- test/verilog/arf/scalers.v
+++ test/verilog/arf/scalers.v
@@ -1,19 +1,19 @@
 `include "parameters.v"
 
-assign 	in_15_1 =   32'd3<<`SHIFT_WIDTH ;
-assign 	in_16_1 =   32'd3<<`SHIFT_WIDTH ;
-assign 	in_17_1 =   32'd3<<`SHIFT_WIDTH ;	
-assign 	in_18_1 =   32'd3<<`SHIFT_WIDTH ;
-assign 	in_21_1 = ~(32'd3<<`SHIFT_WIDTH) + 1 ;
-assign 	in_22_1 = ~(32'd3<<`SHIFT_WIDTH) + 1 ;
-assign 	in_23_1 = ~(32'd3<<`SHIFT_WIDTH) + 1 ;
-assign 	in_24_1 =   32'd3<<`SHIFT_WIDTH ;
+assign 	in_15_1 =   $rtoi( 2.573232*(2**`SHIFT_WIDTH) );
+assign 	in_16_1 =   $rtoi( -1.823232*(2**`SHIFT_WIDTH) );
+assign 	in_17_1 =   $rtoi( 0.732823*(2**`SHIFT_WIDTH) ) ;	
+assign 	in_18_1 =   $rtoi( 1.118323*(2**`SHIFT_WIDTH) ) ;
+assign 	in_21_1 =   $rtoi( -1.232838*(2**`SHIFT_WIDTH) );
+assign 	in_22_1 =   $rtoi( -2.222832*(2**`SHIFT_WIDTH) );
+assign 	in_23_1 =   $rtoi( 1.4733982*(2**`SHIFT_WIDTH) ) ;
+assign 	in_24_1 =   $rtoi( 1.5788023*(2**`SHIFT_WIDTH) ) ;
 	
-assign 	in_1_1 =  32'd3<<`SHIFT_WIDTH ;
-assign 	in_2_1 =  32'd3<<`SHIFT_WIDTH ;
-assign 	in_3_1 =  32'd3<<`SHIFT_WIDTH ;
-assign 	in_4_1 =  32'd3<<`SHIFT_WIDTH ;
-assign 	in_5_1 =  32'd3<<`SHIFT_WIDTH ;
-assign 	in_6_1 =  32'd3<<`SHIFT_WIDTH ;
-assign 	in_7_1 =  32'd3<<`SHIFT_WIDTH ;
-assign 	in_8_1 =  32'd3<<`SHIFT_WIDTH ;
+assign 	in_1_1 =  $rtoi( -2.752332*(2**`SHIFT_WIDTH) );
+assign 	in_2_1 =  $rtoi( 1.283223*(2**`SHIFT_WIDTH) );
+assign 	in_3_1 =  $rtoi( 0.372823*(2**`SHIFT_WIDTH) ) ;	
+assign 	in_4_1 =  $rtoi( 1.181323*(2**`SHIFT_WIDTH) ) ;
+assign 	in_5_1 =  $rtoi( -1.322388*(2**`SHIFT_WIDTH) );
+assign 	in_6_1 =  $rtoi( -2.228232*(2**`SHIFT_WIDTH) );
+assign 	in_7_1 =  $rtoi( 1.4573982*(2**`SHIFT_WIDTH) ) ;
+assign 	in_8_1 =  $rtoi( 1.7588023*(2**`SHIFT_WIDTH) ) ;
