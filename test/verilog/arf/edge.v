a793932cb78fd69108304296c5d87253cfb7040e
me <li3939108@gmail.com>
2014-11-23 18:12:56 -0600

    comments

--- test/verilog/arf/edge.v
+++ test/verilog/arf/edge.v
@@ -1,39 +1,32 @@
-
-
-assign in_9_0  = out_1;
-assign in_9_1  = out_2;
-assign in_10_0 = out_3;
-assign in_10_1 = out_4;
-assign in_11_0 = out_5;
-assign in_11_1 = out_6;
-assign in_12_0 = out_7;
-assign in_12_1 = out_8;
-assign in_27_0 = out_9;
-/* in_13_1 should be constant*/
-assign in_13_0 = out_10;
-/* in_14_1 should be constant*/
-assign in_14_0 = out_11;
-assign in_28_0 = out_12;
-assign in_15_0 = out_13;
-assign in_17_0 = out_13;
-assign in_16_0 = out_14;
-assign in_18_0 = out_14;
-assign in_19_0 = out_15;
-assign in_19_1 = out_16;
-assign in_20_0 = out_17;
-assign in_20_1 = out_18;
-assign in_21_0 = out_19;
-assign in_23_0 = out_19;
-assign in_22_0 = out_20;
-assign in_24_0 = out_20;
-assign in_25_0 = out_21;
-assign in_25_1 = out_22;
-assign in_26_0 = out_23;
-assign in_26_1 = out_24;
-assign in_27_1 = out_25;
-assign in_28_1 = out_26;
-
-
-
-
-
+assign in_9_0  = out_1;           /* [ name = 0 ];   */
+assign in_9_1  = out_2;           /* [ name = 1 ];   */
+assign in_10_0 = out_3;           /* [ name = 2 ];   */
+assign in_10_1 = out_4;           /* [ name = 3 ];   */
+assign in_11_0 = out_5;           /* [ name = 4 ];   */
+assign in_11_1 = out_6;           /* [ name = 5 ];   */
+assign in_12_0 = out_7;           /* [ name = 6 ];   */
+assign in_12_1 = out_8;           /* [ name = 7 ];   */
+assign in_27_0 = out_9;           /* [ name = 8 ];   */
+/* in_13_1 should be constant*/    
+assign in_13_0 = out_10;          /* [ name = 9 ];   */
+/* in_14_1 should be constant*/    
+assign in_14_0 = out_11;          /* [ name = 10 ];  */
+assign in_28_0 = out_12;          /* [ name = 11 ];  */
+assign in_15_0 = out_13;          /* [ name = 12 ];  */
+assign in_17_0 = out_13;          /* [ name = 13 ];  */
+assign in_16_0 = out_14;          /* [ name = 14 ];  */
+assign in_18_0 = out_14;          /* [ name = 15 ];  */
+assign in_19_0 = out_15;          /* [ name = 16 ];  */
+assign in_19_1 = out_16;          /* [ name = 17 ];  */
+assign in_20_0 = out_17;          /* [ name = 18 ];  */
+assign in_20_1 = out_18;          /* [ name = 19 ];  */
+assign in_21_0 = out_19;          /* [ name = 20 ];  */
+assign in_23_0 = out_19;          /* [ name = 21 ];  */
+assign in_22_0 = out_20;          /* [ name = 22 ];  */
+assign in_24_0 = out_20;          /* [ name = 23 ];  */
+assign in_25_0 = out_21;          /* [ name = 24 ];  */
+assign in_25_1 = out_22;          /* [ name = 25 ];  */
+assign in_26_0 = out_23;          /* [ name = 26 ];  */
+assign in_26_1 = out_24;          /* [ name = 27 ];  */
+assign in_27_1 = out_25;          /* [ name = 28 ];  */
+assign in_28_1 = out_26;          /* [ name = 29 ];  */
